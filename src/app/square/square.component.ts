8d5a25def1e607b28ddd8f0a42033ec9d0451e55
keitkalon <79155265+keitkalon@users.noreply.github.com>
2022-04-03 10:50:36 +0300

    boardComponent html logic

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,19 +2,11 @@ import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: `
-    <p>
-      square works!
-      {{ rando }}
-    </p>
+  template: `    
     <button>{{value}}</button>
   `,
   styles: [],
 })
-export class SquareComponent {
-  public rando: number = 0;
-  @Input() value: 'X' | 'O' = 'O';
-  constructor() {
-    setInterval(() => (this.rando = Math.floor(Math.random() * 100)), 500);
-  }
+export class SquareComponent {  
+  @Input() value: 'X' | 'O' = 'O';  
 }
